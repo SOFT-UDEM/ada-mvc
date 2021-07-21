36069687d82acac27f5983d8931d61f2b8e11f0e
Carlos Quintero <carlosquintero95nic@gmail.com>
2021-07-21 15:20:14 -0600

    Add controlador  AccesoLogin

---
+++ ada-mvc/Controllers/AccesoLoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ada_mvc.Controllers
+{
+    public class AccesoLoginController : Controller
+    {
+        // GET: AccesoLogin
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
